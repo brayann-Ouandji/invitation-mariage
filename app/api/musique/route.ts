3ba10faa107930d13ccab508810ab87b2b702740
OUANDJI Brayann <brayann.ouandji@groupe-esigelec.org>
2026-05-15 22:33:58 +0200

    forcer les requetes a ne pas utiliser le cache

--- app/api/musique/route.ts
+++ app/api/musique/route.ts
@@ -1,7 +1,9 @@
+export const dynamic = "force-dynamic";
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
 
 export async function POST(req: NextRequest) {
   try {
